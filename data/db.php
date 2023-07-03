f8f9bd83c7500f2cafb435e8ed86f767009a2b3c
gabrielemafrica <gabriele.ga@live.it>
2023-07-03 21:12:09 +0300

    finale

--- data/db.php
+++ data/db.php
@@ -6,7 +6,7 @@ require_once ("./models/Product.php");
 // categorie
 
 $categoryDog = new Category("dog", "https://cdn-icons-png.flaticon.com/512/194/194279.png");
-$categoryCat = new Category("cat", "https://img.freepik.com/premium-vector/cute-cat-head-cartoon-logo-cat-head-good-cat-care-related-products_487414-285.jpg?w=2000");
+$categoryCat = new Category("cat", "https://us.123rf.com/450wm/iboriz/iboriz1903/iboriz190300009/133955217-gr%C3%A1fico-de-tinta-de-maine-coon-cat-en-vector-dise%C3%B1o-de-tatuaje.jpg");
 
 $categories = [$categoryDog, $categoryCat];
 
@@ -26,10 +26,38 @@ $productGomitolo = new Product ("Gomitolo", 7, "https://img.pixers.pics/pho_wat(
 
 $productRoyalCanin = new Product ("Royal Canin", 40, "https://shop-cdn-m.mediazs.com/bilder/royal/canin/mini/adult/hran/uscat/pentru/cini/aduli/de/ras/mic/9/400/rc_shn_adultmini_mv_eretailkit_ro_ro_9.jpg", "cibo", true, $description3, $categoryDog);
 
-$productKitekat = new Product ("kitekat", 22, "https://shop-cdn-m.mediazs.com/bilder/kitekat/adult/pui/legume/6/400/164398_pla_marspl_kitekat_dryadult_chickenvegetables_hs_01_6.jpg", "cibo", true, $description4, $categoryCat);
+$productKitekat = new Product ("kitekat", 22, "https://shop-cdn-m.mediazs.com/bilder/kitekat/adult/pui/legume/6/400/164398_pla_marspl_kitekat_dryadult_chickenvegetables_hs_01_6.jpg", "cibo", false, $description4, $categoryCat);
 
 $productCucciaConfort = new Product ("Cuccia Confort", 100, "https://www.perilcane.it/3730-large_default/cuccia-coibentata-termica-per-cani-modello-comfort.jpg", "cuccia", true, $description5, $categoryDog);
 
-$productCucciaCat = new Product ("Cuccia Cat", 88, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ38evP9wCCwJF42jEn1pve4bKvU9H-C37h8A&usqp=CAU", "cuccia", true, $description6, $categoryCat);
+$productCucciaCat = new Product ("Cuccia Cat", 88, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ38evP9wCCwJF42jEn1pve4bKvU9H-C37h8A&usqp=CAU", "cuccia", false, $description6, $categoryCat);
 
 $products = [$productOsso, $productGomitolo, $productRoyalCanin, $productKitekat, $productCucciaConfort, $productCucciaCat];
+
+//altri prodotti
+
+$description7 = "Giocattolo interattivo per cani: stimola la mente e mantiene il tuo cane attivo.";
+$description8 = "Giochino a palla per gatti: perfetto per far divertire il tuo felino.";
+$description9 = "Cibo specifico per cani anziani: favorisce la salute delle articolazioni e la vitalità.";
+$description10 = "Cibo umido per gatti: una delizia gustosa e saporita per il tuo micio.";
+$description11 = "Cuccia impermeabile per cani: ideale per proteggere il tuo cane durante le giornate di pioggia.";
+$description12 = "Cuccia a forma di castello per gatti: un luogo incantevole per il sonno e il relax felino.";
+$description13 = "Snack dentale per cani: favorisce l'igiene orale e mantiene il respiro fresco.";
+$description14 = "Giochino con piume per gatti: stimola l'istinto di caccia e intrattiene il tuo micio.";
+$description15 = "Ciotola antiscivolo per cani: previene gli schizzi e mantiene pulito il pavimento.";
+$description16 = "Scratching post per gatti: un punto d'appoggio perfetto per le unghie dei felini.";
+
+$productGiocattoloCane = new Product("Giocattolo per cani", 10, "https://m.media-amazon.com/images/I/71qPpaEidEL._AC_UF894,1000_QL80_.jpg", "gioco", true, $description7, $categoryDog);
+$productPallaGatto = new Product("Palla per gatti", 5, "https://m.media-amazon.com/images/I/61+Y9kA56JL.jpg", "gioco", true, $description8, $categoryCat);
+$productCiboAnziani = new Product("Cibo per cani anziani", 35, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU0JAdX6MgfuvRfxB2yiFCU_fC5RqJR34lfbvbJLELet9JOSoIehNKI7pO0wfGW83poXo&usqp=CAU", "cibo", false, $description9, $categoryDog);
+$productCiboUmidoGatto = new Product("Cibo umido per gatti", 3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpucjXXFQn7WprKbDcNKAkoCjpJJqzs8mHvwEa_wKChgr3vKRjmYgMyHkn33vE_HaAO-I&usqp=CAU", "cibo", true, $description10, $categoryCat);
+$productCucciaImpermeabile = new Product("Cuccia impermeabile per cani", 75, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeGvRStPJnxti8ihl3TYAf92My4JCQNpq8FQ&usqp=CAU", "cuccia", true, $description11, $categoryDog);
+$productCucciaCastelloGatto = new Product("Cuccia a forma di castello per gatti", 55, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwgN_xkibPzQhXaZ_D5XnhTF1ATqmje-gbdegnuPss1vHE6UrNqmelL0ZWraCA4Dzv6Ss&usqp=CAU", "cuccia", true, $description12, $categoryCat);
+$productSnackDentale = new Product("Snack dentale per cani", 8, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlV4O8tb41vj0KO071ESAuz4dZGZUSCghYXCuId7WO7KUmt7hfDBLh70MlXp9RN-pK9l4&usqp=CAU", "snack", false, $description13, $categoryDog);
+$productGiochinoPiume = new Product("Giochino con piume per gatti", 6, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQvaxOsXMd3AUW87jDXx-qrBjmK9UuKyPPKQ&usqp=CAU", "gioco", true, $description14, $categoryCat);
+$productCiotolaAntiscivolo = new Product("Ciotola antiscivolo per cani", 12, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNimSjTA6OLo5xX1DsY8wxp1qtIt9SUjf6lUbNTvvZUOM_CtogdJ-gBlUbwNn0OWOXY50&usqp=CAU", "accessorio", true, $description15, $categoryDog);
+$productScratchingPost = new Product("Scratching post per gatti", 30, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0fAXjqMC-V1cBAwbdCf97Ewk3YTw7YZcngFJ0bHn5mt-XWgtFa4qDrbZbVD-HuwP0ajw&usqp=CAU", "accessorio", true, $description16, $categoryCat);
+
+$additionalProducts = [$productGiocattoloCane, $productPallaGatto, $productCiboAnziani, $productCiboUmidoGatto, $productCucciaImpermeabile, $productCucciaCastelloGatto, $productSnackDentale, $productGiochinoPiume, $productCiotolaAntiscivolo, $productScratchingPost];
+
+$products = array_merge($products, $additionalProducts);
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <div class="container mt-5">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">CANE&GATTO</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -25,22 +25,24 @@
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="#">Shop</a>
                             </li>
+
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                    Categorie
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Dog</a></li>
+                                    <li><a class="dropdown-item" href="#">Cat</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><a class="dropdown-item" href="#">All</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
+                                <a class="nav-link" href="#">Carrello</a>
                             </li>
+
                         </ul>
                         <form class="d-flex" role="search">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -52,31 +54,38 @@
         </div>
     </header>
     <main>
-        <div class="container mt-5">
+        <div class="container mt-5 d-flex flex-wrap gap-5">
             <?php
-                foreach ($variable as $key => $value) {
-                }
-
+                foreach ($products as $key => $value) {
             ?>
-            <div class="card py-3" style="width: 18rem;">
+
+            <div class="card py-2" style="width: 18rem;">
                 <div class="container w-25 border border-secondary">
-                    <img src="<?php echo $productOsso -> getCategory() -> getIcon() ?>" class="card-img-top" alt="<?php echo $productOsso -> getCategory() -> getCategoryName() ?>">
+                    <img src="<?php echo $value -> getCategory() -> getIcon() ?>" class="card-img-top" alt="<?php echo $value -> getCategory() -> getCategoryName() ?>">
                 </div>
-                <img src="<?php echo $productOsso -> getImgURL() ?>" class="card-img-top" alt="<?php echo $productOsso -> getName() ?>">
+                <div class="container w-90 mt-3">
+                    <img src="<?php echo $value -> getImgURL() ?>" class="card-img-top" alt="<?php echo $value -> getName() ?>">
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $productOsso -> getName() ?></h5>
-                    <p class="card-text"><?php echo $productOsso -> getDescription() ?></p>
+                    <h5 class="card-title"><?php echo $value -> getName() ?></h5>
+                    <p class="card-text"><?php echo $value -> getDescription() ?></p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Prezzo <?php echo $productOsso -> getPrice() ?> &euro;</li>
-                    <li class="list-group-item">Categoria <?php echo $productOsso -> getCategory() -> getCategoryName() ?></li>
-                    <li class="list-group-item"><?php echo $productOsso -> getInStock() ? "Disponibile" : "Esaurito"?></li>
+                    <li class="list-group-item">Prezzo: <?php echo $value -> getPrice() ?> &euro;</li>
+                    <li class="list-group-item">Tipologia: <?php echo $value -> getType() ?></li>
+                    <li class="list-group-item">Categoria: <?php echo $value -> getCategory() -> getCategoryName() ?></li>
+                    <li class="<?php echo $value -> getInStock() ? "list-group-item text-success" : "list-group-item text-danger"?>"><?php echo $value -> getInStock() ? "Disponibile" : "Esaurito"?></li>
                 </ul>
                 <div class="card-body">
                     <a href="#" class="card-link">BUY</a>
                     <a href="#" class="card-link">ADD CART</a>
                 </div>
             </div>
+
+            <?php
+                }
+            ?>
+
         </div>
 
     </main>
